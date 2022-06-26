7e6b418aaf458fc11086456b7726f46df2d5d741
dev9998 <81720436+developer9998@users.noreply.github.com>
2022-06-26 19:46:07 -0230

    Initial

---
+++ Source Code/CaveSoundManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+namespace CaveSounds
+{
+    public class CaveSoundManager : MonoBehaviour
+    {
+        static bool canPlaySounds = true;
+        static GameObject cave;
+        static AudioSource caveSoundSource;
+        public void Start()
+        {
+            caveSoundSource = gameObject.AddComponent<AudioSource>();
+            caveSoundSource.minDistance = 20;
+            cave = GameObject.Find("/Level/cave");
+        }
+
+        public void Update()
+        {
+            gameObject.transform.position = GorillaLocomotion.Player.Instance.headCollider.transform.position;
+            if (canPlaySounds)
+            {
+                canPlaySounds = false;
+                StartCoroutine(PlayRandomSound());
+            }
+        }
+
+        IEnumerator PlayRandomSound()
+        {
+            int waitForThisLong = 30 * Random.Range(1, 4);
+            yield return new WaitForSeconds(waitForThisLong);
+
+            bool canPlay = cave.activeInHierarchy;
+            if (canPlay)
+            {
+                int randomChild = Random.Range(0, gameObject.transform.childCount);
+                GameObject obj = gameObject.transform.GetChild(randomChild).gameObject;
+                AudioClip clip = obj.GetComponent<AudioSource>().clip;
+                caveSoundSource.PlayOneShot(clip, 0.5f);
+                yield return new WaitForSeconds(clip.length);
+            }
+            canPlaySounds = true;
+        }
+    }
+}
---
+++ Source Code/CaveSoundPlugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using Utilla;
+
+namespace CaveSounds
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin("com.developer9998.gorillatag.cavesounds", "CaveSounds", "1.0.0")]
+    public class CaveSoundPlugin : BaseUnityPlugin
+    {
+        GameObject soundObject;
+        public void Awake()
+        {
+            Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+            Stream manifestResourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("CaveSounds.cavesounds");
+            AssetBundle assetBundle = AssetBundle.LoadFromStream(manifestResourceStream);
+            soundObject = Instantiate(assetBundle.LoadAsset<GameObject>("Sounds"));
+
+            soundObject.AddComponent<CaveSoundManager>();
+        }
+    }
+}
